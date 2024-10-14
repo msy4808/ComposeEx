d3338deea0dca327fcdac17331665b9cce4301cd
Sungwoon.Moon <swm049@ladaka.com>
2024-10-14 22:14:03 +0900

    Compose LazyColumn 상태 유지 remember -> rememberSaveable 로 변경

--- app/src/main/java/com/moon/composeex/MainActivity.kt
+++ app/src/main/java/com/moon/composeex/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -40,7 +41,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    //remember를 사용하는 대신 rememberSaveable 사용 -> 화면 회전등을 하면 다시 onBoardingScreen이 호출되는걸 방지
+    //화면이 회전등을 해도 상태를 저장함 (remember는 컴포저블이 컴포지션에 유지되는 동안에만 작동. 만약 화면회전을 하면 모든 상태가 손실됨)
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     Column(modifier = modifier.padding(vertical = 4.dp)) {
         if (shouldShowOnboarding) {
@@ -65,7 +68,9 @@ fun Greetings(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val isClick = remember { mutableStateOf(false) }
+    //remember를 사용하는 대신 rememberSaveable 사용 -> 화면 회전등을 하면 버튼 클릭 상태가 초기화되는걸 방지
+    //화면이 회전등을 해도 상태를 저장함 (remember는 컴포저블이 컴포지션에 유지되는 동안에만 작동. 만약 화면회전을 하면 모든 상태가 손실됨)
+    val isClick = rememberSaveable { mutableStateOf(false) }
     val extraPadding = if (isClick.value) 48.dp else 0.dp
 
     Surface(
